af5f71997bd8b4ab23cd00f78043a45ad947dfaa
Errr0 <fatalerror404.6@gmail.com>
2024-11-26 21:14:27 +0100

    functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -13,32 +14,45 @@ void split(std::string str, std::vector<std::string> &arr, char symbol = ' ') {
     arr.push_back(str.substr(start)); // Add the last word
 }
 
-int main(int argc, char* argv[]){
-    std::cout << "Number of arguments: " << argc << std::endl;
+void tokenize(std::string str, std::vector<std::string> &tokens){
+    std::vector<std::string> arr;
+    split(str, arr);
+    
+}
 
-    // Loop through each argument
-    for (int i = 0; i < argc; ++i) {
-        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+bool readfile(std::string filename, std::vector<std::string> &arr){
+    std::fstream file(filename, std::ios::in);
+    if (!file) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 0;
     }
-    ////////////////////////////////////////
+    std::ostringstream buffer;
+    buffer << file.rdbuf();  // Read the entire file buffer into the stream
+    std::string content = buffer.str();
+    file.close();
+    std::cout << content << std::endl;//debug
+    split(content, arr, ';');
+    for (const auto& w : arr) {//debug
+        std::cout << w << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]){
+    //std::cout << "Number of arguments: " << argc << std::endl;
+    //for (int i = 0; i < argc; ++i) {std::cout << "Argument " << i << ": " << argv[i] << std::endl;}
     if(argc<2) {
         std::cerr << "Error no path argument!" << std::endl;
         return 1;
     }
-    std::fstream file(argv[1], std::ios::in);
-    if (!file) {
-        std::cerr << "Error opening file!" << std::endl;
-        return 2;
-    }
-    std::string line;
-    std::vector<std::string> tokens;
-    std::getline(file, line);
-    std::cout << line << "\n";
-    split(line, tokens);
+    std::vector<std::string> statements;
+    if(!readfile(argv[1], statements)) return 2;
+
+    //std::vector<std::string> tokens;
+    //std::getline(file, line);
+    //std::cout << line << "\n";
+    //split(line, tokens);
     // Print the result
-    for (const auto& w : tokens) {
-        std::cout << w << std::endl;
-    }
+
 
     return 0;
 }
