ef26408e465c2fc9608712b3f162e2251a29a946
Errr0 <119783076+Errr0@users.noreply.github.com>
2024-11-27 15:10:22 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <regex>
 
 void split(std::string str, std::vector<std::string> &arr, char symbol = ' ') {
-    int start = 0, end = str.find(' ');
+    size_t start = 0, end = str.find(symbol);//int with different name
     while (end != std::string::npos) {
         arr.push_back(str.substr(start, end - start));
         start = end + 1; // Move past the space
@@ -14,13 +15,17 @@ void split(std::string str, std::vector<std::string> &arr, char symbol = ' ') {
     arr.push_back(str.substr(start)); // Add the last word
 }
 
+void replace(std::string &str, std::string from, std::string to) {
+    str = std::regex_replace(str, std::regex(from), to);
+}
+
 void tokenize(std::string str, std::vector<std::string> &tokens){
     std::vector<std::string> arr;
     split(str, arr);
     
 }
 
-bool readfile(std::string filename, std::vector<std::string> &arr){
+bool readfile(std::string filename, std::vector<std::string> &statements){
     std::fstream file(filename, std::ios::in);
     if (!file) {
         std::cerr << "Error opening file!" << std::endl;
@@ -30,11 +35,13 @@ bool readfile(std::string filename, std::vector<std::string> &arr){
     buffer << file.rdbuf();  // Read the entire file buffer into the stream
     std::string content = buffer.str();
     file.close();
-    std::cout << content << std::endl;//debug
-    split(content, arr, ';');
-    for (const auto& w : arr) {//debug
+    //std::cout << content << std::endl;//debug
+    split(content, statements, ';');
+    for (auto& w : statements) {//debug
+        replace(w, "\n", " ");
         std::cout << w << std::endl;
     }
+    return 1;
 }
 
 int main(int argc, char* argv[]){
