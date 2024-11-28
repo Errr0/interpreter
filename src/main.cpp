fa96a9d100bac506d565acef26a0a90497c687f2
Errr0 <119783076+Errr0@users.noreply.github.com>
2024-11-28 13:21:52 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,60 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <array>
+#include <map>
 #include <regex>
 
 enum TokenType {
-    END = 0
+    END,
+    //arithmetics
+    ASSIGN,
+    PLUS,
+    MINUS,
+    ASTERISK,
+    SLASH,
+    MULTIPLY,
+    DIVIDE,
+    ADD,
+    SUBTRACT,
+    //logic
+    NOT,
+    AND,
+    OR,
+    EQUAL,
+    NOTEQUAL,
+    LESSTHAN,
+    LESSEQUAL,
+    GREATERTHAN,
+    GREATEREQUAL,
+
+    INT
+};
+
+std::map<std::string, TokenType> map = {
+    {"=", ASSIGN},
+    {"+", PLUS},
+    {"-", MINUS},
+    {"*", ASTERISK},
+    {"/", SLASH},
+    {"+=", ADD},
+    {"-=", SUBTRACT},
+    {"*=", MULTIPLY},
+    {"/=", DIVIDE},
+    
+    {"!", NOT},
+    {"&&", AND},
+    {"||", OR},
+    {"==", EQUAL},
+    {"!=", NOTEQUAL},
+    {"<", LESSTHAN},
+    {"<=", LESSEQUAL},
+    {">", GREATERTHAN},
+    {">=", GREATEREQUAL},
 };
 
 class Token{
+    public:
     std::string value;
     enum TokenType type;
     Token(std::string value = "",enum TokenType type = END){
@@ -23,26 +70,82 @@ void split(std::string str, std::vector<std::string> &arr, char symbol = ' ') {
     while (end != std::string::npos) {
         if(start != end)
         arr.push_back(str.substr(start, end - start));
-        start = end + 1; // Move past the space
+        start = end + 1;
         end = str.find(symbol, start);
     }
     if(str.substr(start) != " " && str.substr(start) != "")
     arr.push_back(str.substr(start)); // Add the last word
 }
 
+bool splitChunks(std::string str, std::vector<std::string> &arr) {
+    std::array<std::string, 3> symbols = {"+=","-=","="};
+    size_t start, end;
+    bool change = false;
+    for (std::string s : symbols) {
+        start = 0;
+        end = str.find(s);
+        if(change) continue;
+        while (end < std::string::npos) {
+            if(start != end){
+                arr.push_back(str.substr(start, end - start));
+                arr.push_back(str.substr(end - start, s.length()));
+                change = true;
+            }
+            start = end + (s.length()>0 ? s.length() : 1);
+            end = str.find(s, start);
+            //std::cout << " | 1 | "<< end << " | " <<start<< " | "<< s <<" | "<<s.length()<<" | "<< (s.length()>0 ? s.length() : 1) << " | \n";
+        }
+        if(str.substr(start) != " " && str.substr(start) != "" && change){
+            arr.push_back(str.substr(start));
+        } 
+    }
+    return change;
+}
+
 void replace(std::string &str, std::string from, std::string to) {
     str = std::regex_replace(str, std::regex(from), to);
 }
 
-void parse(std::string str, std::vector<Token> &tokens){
+void makeToken(std::string &str, std::vector<Token> &tokens){  
+}
+
+void tokenize(std::vector<std::string> arr, std::vector<Token> &tokens){
+    for (std::string str : arr) {
+        if(str.length()<=2){
+            tokens.push_back(Token(str,map[str]));
+        } else if(1){
+            continue;
+        } else if(1){
+            continue;
+        } else if(1){
+            continue;
+        } else if(1){
+            continue;
+        } else if(1){
+            continue;
+        }
+    }
+    tokens.push_back(Token());
+}
+
+void parse(std::string &str, std::vector<Token> &tokens){
     std::vector<std::string> words;
     replace(str, "\n", " ");
     split(str, words);
+    std::vector<std::string> arr;
     for (std::string w : words) {
-        std::cout << w << std::endl;
-            //tokens.push_back("END");
+        //std::cout <<"words: |" << w <<"|" << std::endl;
+        if(!splitChunks(w, arr)){
+            arr.push_back(w);
+        }
     }
-}
+    //arr.push_back("");
+    tokenize(arr, tokens);
+    for (std::string w : arr) {//debug
+        std::cout <<"arr: |" << w <<"|" << std::endl;
+    }
+    
+}            //tokens.push_back("END");
 
 bool readfile(std::string filename, std::vector<std::string> &statements){
     std::fstream file(filename, std::ios::in);
@@ -57,11 +160,13 @@ bool readfile(std::string filename, std::vector<std::string> &statements){
     //std::cout << content << std::endl;//debug
     std::vector<Token> tokens;
     split(content, statements, ';');
-
-    for (auto& w : statements) {//debug
+    
+    for (std::string& w : statements) {//debug
         parse(w, tokens);
-
         //std::cout << " statement: "<< w << std::endl;
+    }
+    for (Token& w : tokens) {
+        std::cout << " token: "<< w.type << " value: " << w.value << std::endl;
     }
     return 1;
 }
